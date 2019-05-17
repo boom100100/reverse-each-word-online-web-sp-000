15c9b4b4b629ee80a630c8b5e4ded8ab8f4a3913
Bernadette Davis <bsd231@nyu.edu>
2019-05-17 14:18:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentenceArray = sentence.split(" ")
 
-  sentenceArray.each_with_index do {|word, index|
+  sentenceArray.each_with_index {|word, index|
     sentenceArray[index] = word.reverse
   }
 
